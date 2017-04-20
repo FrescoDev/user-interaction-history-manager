4b52a60aed9673f48b1c6c99475f4880d25185be
frescodev <frescodev@kojos-iMac.local>
2017-04-20 23:40:41 +0100

    neatens up cache files

--- uihm.component/cache/index.js
+++ uihm.component/cache/index.js
@@ -1,5 +1,14 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./client.prod')
-} else {
-  module.exports = require('./client.dev')
-}
+const redis = (process.env.NODE_ENV === 'production') ? require('redis') : require('redis-mock');
+const cachingClient = redis.createClient();
+
+cachingClient.on('error', (err) => {
+  // eslint-disable-next-line no-console      
+  console.log('   Error connecting to Redis cache :(' + err)
+});
+
+cachingClient.on('connect', () => {
+  // eslint-disable-next-line no-console  
+  console.log('    Woo Hoo! Redis connected!\n')
+});
+
+export default cachingClient;
