d6d9a34c35948dd15c219778af3baea4a68182de
OldFresco <hinsonkojo@gmail.com>
2017-04-23 01:20:40 +0100

    add redis env vars

--- uihm.component/cache/index.js
+++ uihm.component/cache/index.js
@@ -1,14 +1,16 @@
+import config from '../configuration';
+
 const redis = (process.env.NODE_ENV === 'production') ? require('redis') : require('redis-mock');
-const cachingClient = redis.createClient();
+const cachingClient = redis.createClient(config.redis.url, { no_ready_check: true });
 
 cachingClient.on('error', (err) => {
-  // eslint-disable-next-line no-console      
-  console.log('   Error connecting to Redis cache :(' + err)
+    // eslint-disable-next-line no-console      
+    console.log('   Error connecting to Redis cache :(' + err)
 });
 
 cachingClient.on('connect', () => {
-  // eslint-disable-next-line no-console  
-  console.log('    Woo Hoo! Redis connected!\n')
+    // eslint-disable-next-line no-console  
+    console.log('    Woo Hoo! Redis connected!\n')
 });
 
 export default cachingClient;
--- uihm.component/configuration/index.js
+++ uihm.component/configuration/index.js
@@ -1,31 +1,36 @@
-import {merge} from 'lodash';
+import { merge } from 'lodash';
 import path from 'path';
 
 // Default configuations applied to all environments
 const defaultConfig = {
-  env: process.env.NODE_ENV,
-  get envs() {
-    return {
-      test: process.env.NODE_ENV === 'test',
-      development: process.env.NODE_ENV === 'development',
-      production: process.env.NODE_ENV === 'production'
+    env: process.env.NODE_ENV,
+    get envs() {
+        return {
+            test: process.env.NODE_ENV === 'test',
+            development: process.env.NODE_ENV === 'development',
+            production: process.env.NODE_ENV === 'production'
+        }
+    },
+    root: path.normalize(__dirname + '/../../..'),
+    port: process.env.PORT || 4567,
+    ip: process.env.IP || '0.0.0.0',
+    redis: {
+        host: 'localhost',
+        port: '3000',
+        url: 'http://localhost:3000'
     }
-  },
-  root: path.normalize(__dirname + '/../../..'),
-  port: process.env.PORT || 4567,
-  ip: process.env.IP || '0.0.0.0'
 };
 
 // Environment specific overrides
 const environmentConfigs = {
-  development: {},
-  test: {
-    port: 5678,
-    security: {
-      saltRounds: 4
-    }
-  },
-  production: {}
+    development: {},
+    test: {
+        port: 5678,
+        security: {
+            saltRounds: 4
+        }
+    },
+    production: {}
 };
 
 // Recursively merge configurations
